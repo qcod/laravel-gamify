3c967d3c615998ae5863ba3c6aa2b35495458b1e
Lloric Mayuga Garcia <lloricode@gmail.com>
2023-08-03 08:44:29 +0800

    ide helper
    Signed-off-by: Lloric Mayuga Garcia <lloricode@gmail.com>

--- tests/Fixtures/Models/Reply.php
+++ tests/Fixtures/Models/Reply.php
@@ -5,19 +5,25 @@ declare(strict_types=1);
 namespace QCod\Gamify\Tests\Fixtures\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property  int $id
+ * @property-read \QCod\Gamify\Tests\Fixtures\Models\User $user
+ * @property-read \QCod\Gamify\Tests\Fixtures\Models\Post $post
+ */
 class Reply extends Model
 {
     public $table = 'test_replies';
 
     protected $guarded = [];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function post()
+    public function post(): BelongsTo
     {
         return $this->belongsTo(Post::class);
     }
--- tests/Fixtures/Models/User.php
+++ tests/Fixtures/Models/User.php
@@ -5,18 +5,24 @@ declare(strict_types=1);
 namespace QCod\Gamify\Tests\Fixtures\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use QCod\Gamify\Gamify;
 
+/**
+ * @property  int $id
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \QCod\Gamify\Badge> $badges
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \QCod\Gamify\Reputation> $reputations
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \QCod\Gamify\Tests\Fixtures\Models\Post> $posts
+ */
 class User extends Model
 {
     use Gamify;
 
-    // used in config to alter a project level model
     public $table = 'test_users';
 
     protected $guarded = [];
 
-    public function posts()
+    public function posts(): HasMany
     {
         return $this->hasMany(Post::class);
     }
